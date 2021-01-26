6d272c9c13b09be99ca81d1728cba44a3f7ce0f7
Brandon Myers <brandonRmyers1512@gmail.com>
2021-01-26 15:41:23 -0800

    Readding bio pic

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,5 @@
-import { Container, Divider, Icon, Header } from 'semantic-ui-react';
-// import ProfilePic from '../assets/profile-pic.jpg';
+import { Container, Divider, Icon, Header, Image} from 'semantic-ui-react';
+import profile from '../assets/profile-pic.jpg';
 import SkillList from '../components/SkillList';
 
 function Bio (props) {
@@ -11,7 +11,7 @@ function Bio (props) {
                     Biography
                 </Header>
             </Divider>
-            {/* <Image src={ProfilePic} size='medium' circular centered /> */}
+            <Image src={profile} size='medium' circular centered />
                 <Divider hidden />
             <Container text>
             <h3> Hello, my name is Brandon Robert Myers. I am a software engineer / web developer based in California. I love creating and building things with code.  My goal is to create efficient and reliable software, while always learning and growing. </h3> 
